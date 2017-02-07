b55e407552f4d64573d0f1a8dde8cb18dd80a8cf
olegnovatskiy <olegnovatskiy@gmail.com>
2017-02-07 12:48:03 +0200

    Initialization project

---
+++ src/main/java/com/declarationapi/DeclarationApiApplication.java
@@ -0,0 +1,12 @@
+package com.declarationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeclarationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeclarationApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/declarationapi/DeclarationApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.declarationapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DeclarationApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
